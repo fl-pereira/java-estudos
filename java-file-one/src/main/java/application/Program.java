89cecd28be7e76fb1f7d4ac3e9225bba984351de
fl-pereira <fpereira.media@gmail.com>
2022-10-27 14:17:32 -0300

    sync

--- java-file-one/src/main/java/application/Program.java
+++ java-file-one/src/main/java/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import entities.Product;
+
+import java.io.*;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -13,18 +16,50 @@ import java.util.Scanner;
  */
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
-        String path = "c:\\Temp\\in.txt";
-        try(BufferedReader br = new BufferedReader(new FileReader(path))){
-            String line = br.readLine();
-            while(line != null){
-                System.out.println(line);
-                line = br.readLine();
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Product> list = new ArrayList<>();
+
+        System.out.println("Enter file path: ");
+        String sourceFileStr = sc.nextLine();
+
+        File sourceFile = new File(sourceFileStr);
+        String folderStr = sourceFile.getParent();
+        System.out.println(folderStr);
+
+        boolean success = new File(folderStr + "\\out").mkdir();
+
+        String targetFileStr = folderStr + "\\out\\summary.csv";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))){
+            String itemCsv = br.readLine();
+            while (itemCsv != null) {
+                String[] fields = itemCsv.split(",");
+                String name = fields[0];
+                double price = Double.parseDouble(fields[1]);
+                int quantity = Integer.parseInt(fields[2]);
+                list.add(new Product(name,price,quantity));
+
+                itemCsv = br.readLine();
+            }
+
+            try(BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))){
+                for(Product item : list){
+                    bw.write(item.getName() + "," + String.format("%.2f", item.total()));
+                    bw.newLine();
+                }
+                System.out.println(targetFileStr + " CREATED");
+            }
+            catch (IOException e){
+                System.out.println("Error writing file: " + e.getMessage());
             }
         }
-        catch(IOException e){
-            System.out.println("Error: " + e.getMessage());
+        catch (IOException e){
+            System.out.println("Error writing file: " + e.getMessage());
         }
+        sc.close();
     }
 }
