c7952afdd7864107106392a76d3a7938e8ac0728
fl-pereira <fpereira.media@gmail.com>
2022-10-27 12:26:11 -0300

    sync

---
+++ java-expections-two/src/main/java/application/JavaExpectionsTwo.java
@@ -0,0 +1,13 @@
+package application;
+
+/**
+ *
+ * @author Felipe Pereira
+ * 
+ */
+public class JavaExpectionsTwo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
