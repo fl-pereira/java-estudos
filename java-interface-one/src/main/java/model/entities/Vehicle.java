5466e3aa3dcbb0d9445d93aac5efdc11f61b43e5
fl-pereira <fpereira.media@gmail.com>
2022-10-28 14:51:15 -0300

    sync

---
+++ java-interface-one/src/main/java/model/entities/Vehicle.java
@@ -0,0 +1,28 @@
+package model.entities;
+
+/**
+ *
+ * @author Felipe Pereira
+ * 
+ */
+public class Vehicle {
+    
+    private String model;
+    
+    public Vehicle(){
+        
+    }
+    
+    public Vehicle(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+    
+}
