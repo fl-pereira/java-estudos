a01558ec22da6688217698484cb8a13030b2d032
fl-pereira <fpereira.media@gmail.com>
2022-11-07 08:09:56 -0300

    sync

--- java-interface-one/src/main/java/application/Program.java
+++ java-interface-one/src/main/java/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import model.entities.CarRental;
 import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -29,12 +31,21 @@ public class Program {
         LocalDateTime start = LocalDateTime.parse(sc.next(), fmt);
         System.out.print("Retorno (dd/mm/aaaa hh:mm): ");
         LocalDateTime finish = LocalDateTime.parse(sc.next(), fmt);
-        System.out.print("Insira o preço por hora: ");
-        double valuePerHour = sc.nextDouble();
-        System.out.println("Insira o preço por dia: ");
-        double valuePerDay = sc.nextDouble();
 
         CarRental cr = new CarRental(start,finish,new Vehicle(modelCar));
+
+        System.out.print("Insira o preço por hora: ");
+        double valuePerHour = sc.nextDouble();
+        System.out.print("Insira o preço por dia: ");
+        double valuePerDay = sc.nextDouble();
+
+        RentalService rentalService = new RentalService(valuePerHour,valuePerDay,new BrazilTaxService());
+        rentalService.processInvoice(cr);
+
+        System.out.println("### FATURA ###");
+        System.out.println("Pagamento base: " + cr.getInvoice().getBasicPayment());
+        System.out.println("Impost: " + cr.getInvoice().getTax());
+        System.out.println("Pagamento total: " + cr.getInvoice().getTotalPayment());
 
         sc.close();
     }
