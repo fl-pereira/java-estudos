5bd44cc238d89012e0e8bd3538c175aaea375a26
Felipe Pereira <fpereira.media@gmail.com>
2022-12-22 19:21:25 -0300

    sync

--- java-bancodedados-one/src/main/java/application/Program.java
+++ java-bancodedados-one/src/main/java/application/Program.java
@@ -3,10 +3,31 @@ package application;
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
-    public static void main(String[] args){
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+    public static void main(String[] args) throws SQLException {
+        Connection conn = null;
+        Statement st = null;
+        ResultSet res = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            res = st.executeQuery("select * from department");
+
+            while (res.next()) {
+                System.out.println(res.getInt("Id") + ", " + res.getString("Name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            res.close();
+            st.close();
+            DB.closeConnection();
+        }
+
     }
 }
--- java-bancodedados-one/src/main/java/db/DB.java
+++ java-bancodedados-one/src/main/java/db/DB.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
