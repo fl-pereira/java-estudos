e3d3642c49ea0d6fc2bf397970d97a2ee51ed1bd
Felipe Pereira <fpereira.media@gmail.com>
2022-12-26 11:15:32 -0300

    findById

--- java-dao-jdbc/src/main/java/application/Program.java
+++ java-dao-jdbc/src/main/java/application/Program.java
@@ -1,8 +1,22 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+
+import java.sql.SQLException;
 
 public class Program {
 
-
+    public static void main(String[] args){
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = null;
+        try {
+            seller = sellerDao.findById(3);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(seller);
+    }
 }
--- java-dao-jdbc/src/main/java/model/dao/SellerDao.java
+++ java-dao-jdbc/src/main/java/model/dao/SellerDao.java
@@ -1,6 +1,8 @@
 package model.dao;
 
 import entities.Seller;
+
+import java.sql.SQLException;
 import java.util.List;
 
 public interface SellerDao {
@@ -8,7 +10,7 @@ public interface SellerDao {
     void insert(Seller obj);
     void update(Seller obj);
     void deleteById(Integer id);
-    Seller findById(Integer id);
+    Seller findById(Integer id) throws SQLException;
     List<Seller> findAll();
 
 }
