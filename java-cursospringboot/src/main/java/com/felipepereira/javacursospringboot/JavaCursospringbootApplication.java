330c628ba6584bdda1561562416a8c9486ce9911
Felipe Pereira <fpereira.media@gmail.com>
2022-12-27 10:20:27 -0300

    sync

---
+++ java-cursospringboot/src/main/java/com/felipepereira/javacursospringboot/JavaCursospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.felipepereira.javacursospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCursospringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCursospringbootApplication.class, args);
+    }
+
+}
